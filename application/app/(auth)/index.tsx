4529db2a613a7bc75a6ebe4a40f4550456f553cb
Ukhang <ukhangmarma459@gmail.com>
2025-02-18 12:59:01 +0600

    using new components in sign in screen

--- application/app/(auth)/index.tsx
+++ application/app/(auth)/index.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Button from "@/components/ui/button";
 import TextInput from "@/components/ui/text-input";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
+import { View } from "react-native";
 
 export default function SignIn() {
   const { signIn, setActive, isLoaded } = useSignIn();
@@ -12,12 +13,54 @@ export default function SignIn() {
 
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
-  const [isSigningIn, setIsSigningIn] = useState("");
+  const [isSigningIn, setIsSigningIn] = useState(false);
+
+  const onSignInPress = () => {
+
+  };
 
   return (
-    <BodyScrollView>
-      <TextInput label=""/>
-      <Button>Hello</Button>
+    <BodyScrollView contentContainerStyle={{
+      padding: 16,
+    }}>
+      <TextInput
+        label="Email"
+        value={emailAddress}
+        placeholder="Enter email"
+        autoCapitalize="none"
+        keyboardType="email-address"
+        onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
+      />
+      <TextInput
+        value={password}
+        label="Password"
+        placeholder="Enter password"
+        secureTextEntry={true}
+        onChangeText={(password) => setPassword(password)}
+      />
+       <Button
+        onPress={onSignInPress}
+        loading={isSigningIn}
+        disabled={!emailAddress || !password || isSigningIn}
+      >
+        Sign in
+      </Button>
+
+      <View style={{ marginTop: 16, alignItems: "center" }}>
+        <ThemedText>Don't have an account?</ThemedText>
+        <Button onPress={() => router.push("/sign-up")} variant="ghost">
+          Sign up
+        </Button>
+      </View>
+      <View style={{ marginTop: 16, alignItems: "center" }}>
+        <ThemedText>Forgot password?</ThemedText>
+        <Button
+          onPress={() => router.push("/reset-password")}
+          variant="ghost"
+        >
+          Reset password
+        </Button>
+      </View>
     </BodyScrollView>
   );
 }
