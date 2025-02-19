ae21f15b12b5867bfe224251c3168f8ce16d7d2d
Ukhang <ukhangmarma459@gmail.com>
2025-02-19 10:26:45 +0600

    customizing list screens presentation

--- application/app/(index)/list/new/index.tsx
+++ application/app/(index)/list/new/index.tsx
@@ -1,10 +1,81 @@
 import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
+import { StyleSheet } from "react-native";
 
 export default function NewListScreen() {
-    return (
-        <BodyScrollView>
-            <ThemedText></ThemedText>
-        </BodyScrollView>
-    )
+  return (
+    <BodyScrollView contentContainerStyle={styles.scrollViewContent}>
+      <ThemedText type="subtitle" style={styles.title}>
+        Better Together
+      </ThemedText>
+      <ThemedText type="defaultSemiBold" style={styles.subtitle}>
+        Create shared shopping lists and collaborate in real-time with family
+        and friends
+      </ThemedText>
+    </BodyScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  scrollViewContent: {
+    padding: 16,
+    marginBottom: 100,
+  },
+  container: {
+    gap: 32,
+  },
+  heroSection: {
+    alignItems: "center",
+    gap: 16,
+    marginTop: 32,
+  },
+  iconCircle: {
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 32,
+    textAlign: "center",
+  },
+  subtitle: {
+    textAlign: "center",
+    color: "gray",
+    paddingHorizontal: 24,
+    lineHeight: 24,
+  },
+  actionSection: {
+    gap: 24,
+  },
+  buttonIcon: {
+    marginRight: 8,
+  },
+  divider: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+  },
+  line: {
+    flex: 1,
+    height: 1,
+    backgroundColor: "rgba(150, 150, 150, 0.2)",
+  },
+  orText: {
+    color: "gray",
+  },
+  joinSection: {
+    gap: 16,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 16,
+  },
+  textInput: {
+    flex: 1,
+  },
+  qrButton: {
+    marginBottom: 16,
+  },
+  joinButton: {
+    marginTop: 8,
+  },
+});
