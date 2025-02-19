72826603430282254519a023a1b7543bf4beca3c
Ukhang <ukhangmarma459@gmail.com>
2025-02-19 16:36:30 +0600

    creating shoppingListStore component

---
+++ application/stores/ShoppingListStore.tsx
@@ -0,0 +1,63 @@
+import * as UiReact from "tinybase/ui-react/with-schemas";
+
+const STORE_ID_PREFIX = "shoppingListStore-";
+
+const VALUES_SCHEMA = {
+  listId: { type: "string" },
+  name: { type: "string" },
+  description: { type: "string" },
+  emoji: { type: "string" },
+  color: { type: "string" },
+  createdAt: { type: "string" },
+  updatedAt: { type: "string" },
+} as const;
+const TABLES_SCHEMA = {
+  products: {
+    id: { type: "string" },
+    name: { type: "string" },
+    quantity: { type: "number" },
+    units: { type: "string" },
+    isPurchased: { type: "boolean", default: false },
+    category: { type: "string", default: "" },
+    notes: { type: "string" },
+    createdBy: { type: "string" }, // userId
+    createdAt: { type: "string" },
+    updatedAt: { type: "string" },
+  },
+  collaborators: {
+    nickname: { type: "string" },
+  },
+} as const;
+
+type Schemas = [typeof TABLES_SCHEMA, typeof VALUES_SCHEMA];
+type ShoppingListValueId = keyof typeof VALUES_SCHEMA;
+type ShoppingListProductCellId = keyof (typeof TABLES_SCHEMA)["products"];
+
+const {
+  useCell,
+  useCreateMergeableStore,
+  useDelRowCallback,
+  useProvideRelationships,
+  useProvideStore,
+  useRowCount,
+  useSetCellCallback,
+  useSetValueCallback,
+  useSortedRowIds,
+  useStore,
+  useCreateRelationships,
+  useTable,
+  useValue,
+  useValuesListener,
+} = UiReact as UiReact.WithSchemas<Schemas>;
+
+const useStoreId = (listId: string) => STORE_ID_PREFIX + listId;
+
+export default function ShoppingListStore({
+  listId,
+  initialContentJson
+}: {
+  listId: string;
+  initialContentJson: string;
+}) {
+  
+}
